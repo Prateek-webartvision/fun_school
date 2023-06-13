c2875e4e7f86a17929d1374ec619ccb742d15bd4
Kundan mandal <45583046+kundanm881@users.noreply.github.com>
2023-06-13 13:08:18 +0530

    update

--- lib/ui/detail_exam_page/detail_exam_page.dart
+++ lib/ui/detail_exam_page/detail_exam_page.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:citycloud_school/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:kd_utils/kd_utils.dart';
 
@@ -105,7 +106,119 @@ class _DetailExamPageState extends State<DetailExamPage> with TickerProviderStat
               ],
             ),
           ),
+
+          //body
+          Expanded(
+              child: TabBarView(
+            controller: pageTabController,
+            children: [
+              _RealTabView(),
+              _RealTabView(),
+              _RealTabView(),
+            ],
+          ))
         ],
+      ),
+    );
+  }
+}
+
+class _RealTabView extends StatelessWidget {
+  // const _RealTabView({
+  //   super.key,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(16),
+      children: [
+        _ExamTile(
+          completeState: 1,
+          onClick: () {
+            print("object ${appRoutes.location}");
+          },
+        ),
+        12.height,
+        _ExamTile(completeState: 2),
+        12.height,
+        _ExamTile(completeState: 3),
+      ],
+    );
+  }
+}
+
+class _ExamTile extends StatelessWidget {
+  const _ExamTile({
+    super.key,
+    required this.completeState,
+    this.onClick,
+  });
+
+  final int completeState;
+  final Function()? onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onClick,
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColor.white,
+          border: Border.all(color: AppColor.softBorderColor),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  // height: 24,
+                  decoration: BoxDecoration(
+                    color: AppColor.scaffoldBg,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.black),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  child: Text(
+                    "Question $completeState/7",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: AppColor.scaffoldBg,
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: AppColor.textFeildBorderColor),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  child: Text(
+                    "1 point",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            8.height,
+            Text(
+              "Of the following, which best describes a variable?",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
