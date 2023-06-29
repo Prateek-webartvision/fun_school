8ccb4e41e46d5d6833c36e7eb29781f0a583b851
Kundan mandal <45583046+kundanm881@users.noreply.github.com>
2023-06-29 10:07:03 +0530

    pub update

--- lib/repo/quiz_repo/quiz_repo.dart
+++ lib/repo/quiz_repo/quiz_repo.dart
@@ -3,7 +3,7 @@ import 'package:citycloud_school/network/url/app_urls.dart';
 import '../../network/app_api.dart';
 
 class QuizRepository {
-  static final _api = AppApi();
+  // static final _api = AppApi();
 
   static Future getQuiz({String? title = ""}) async {
     print(AppUrls.getQuizs);
